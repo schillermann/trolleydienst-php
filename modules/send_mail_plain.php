9845521d6d55cc9193c58b0e226527cd92acb52c
Mike Copestake <hello@plnt.co.uk>
2022-07-08 12:37:02 +0100

    SMTP & SendinBlue Mail
    Added SMTP & SendinBlue as options for sending mail

--- modules/send_mail_plain.php
+++ modules/send_mail_plain.php
@@ -1,10 +1,78 @@
 <?php
 return function ($to, $subject, $message): bool {
+    if ( SMTP == "php" ) {
+        return function ($to, $subject, $message): bool {
+            $headers   = [];
+            $headers[] = 'Content-type: text/plain; charset=utf-8';
+            $headers[] = 'From: =?UTF-8?B?'.base64_encode(APPLICATION_NAME . ' - ' . CONGREGATION_NAME).'?=<' . EMAIL_ADDRESS_FROM . '>';
+            $headers[] = 'Reply-To: ' . TEAM_NAME . ' <' . EMAIL_ADDRESS_REPLY . '>';
+        
+            return mail($to, $subject, $message, implode("\r\n",$headers));
+        };
+    } else if ( SMTP == "smtp" ) {
+        //Load Composer's autoloader
+        require_once('../vendor/autoload.php');
 
-    $headers   = [];
-    $headers[] = 'Content-type: text/plain; charset=utf-8';
-    $headers[] = 'From: =?UTF-8?B?'.base64_encode(APPLICATION_NAME . ' - ' . CONGREGATION_NAME).'?=<' . EMAIL_ADDRESS_FROM . '>';
-    $headers[] = 'Reply-To: ' . TEAM_NAME . ' <' . EMAIL_ADDRESS_REPLY . '>';
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\Exception;
 
-    return mail($to, $subject, $message, implode("\r\n",$headers));
+        require '../vendor/PHPMailer/src/Exception.php';
+        require '../vendor/PHPMailer/src/PHPMailer.php';
+        require '../vendor/PHPMailer/src/SMTP.php';
+
+        $mail = new PHPMailer;
+
+        //Server settings
+        if ( SMTP_DEBUG) {
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;              // Enable verbose debug output
+        }
+        $mail->Host       = SMTP_HOST;                          //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                               //Enable SMTP authentication
+        $mail->Username   = SMTP_USERNAME;                      //SMTP username
+        $mail->Password   = SMTP_PASSWORD;                      //SMTP password
+        if ( SMTP_ENCRYPTION == "ssl" ) {
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;    //Enable implicit TLS encryption
+        } else if ( SMTP_ENCRYPTION == "tls") {
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        }
+        $mail->Port       = SMTP_PORT;                          //TCP port to connect to
+
+        //Recipients
+        $mail->setFrom(EMAIL_ADDRESS_FROM, CONGREGATION_NAME . ' - ' . APPLICATION_NAME);
+        $mail->addAddress($to);
+        $mail->addReplyTo(EMAIL_ADDRESS_REPLY, TEAM_NAME);
+        $mail->addBCC(EMAIL_ADDRESS_BCC);
+        
+        //Content
+        $mail->isHTML(true);                                    //Set email format to HTML
+        $mail->Subject = $subject;
+        $mail->Body    = '<html><body>' . str_replace("||", "<br>", $message) . '</body></html>';
+
+        if(!$mail->send()) {
+            return false;
+        } else {
+            return true;
+        }
+    } else if ( SMTP == "sendinblue" ) {
+        //Load Composer's autoloader
+        require_once('../vendor/autoload.php');
+
+        $credentials = SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKey('api-key', MAIL_SENDINBLUE_API_KEY);
+        $apiInstance = new SendinBlue\Client\Api\TransactionalEmailsApi(new GuzzleHttp\Client(),$credentials);
+        
+        $sendSmtpEmail = new \SendinBlue\Client\Model\SendSmtpEmail([
+            'subject' => $subject,
+            'sender' => ['name' => CONGREGATION_NAME . ' - ' . APPLICATION_NAME, 'email' => EMAIL_ADDRESS_FROM],
+            'replyTo' => ['name' => TEAM_NAME, 'email' => EMAIL_ADDRESS_REPLY],
+            'to' => [[ 'email' => $to]],
+            'htmlContent' => '<html><body>' . str_replace("||", "<br>", $message) . '</body></html>'
+       ]);
+    
+       try {
+            $result = $apiInstance->sendTransacEmail($sendSmtpEmail);
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
 };
---
+++ updates/1.11.0.php
@@ -0,0 +1,28 @@
+<?php return function (\PDO $connection): bool {
+
+    $config = [
+        'EMAIL_ADDRESS_FROM' => EMAIL_ADDRESS_FROM,
+        'EMAIL_ADDRESS_REPLY' => EMAIL_ADDRESS_REPLY,
+        'CONGREGATION_NAME' => CONGREGATION_NAME,
+        'APPLICATION_NAME' => APPLICATION_NAME,
+        'TEAM_NAME' => TEAM_NAME,
+        'UPLOAD_SIZE_MAX_IN_MEGABYTE' => UPLOAD_SIZE_MAX_IN_MEGABYTE,
+        'BAN_TIME_IN_MINUTES' => BAN_TIME_IN_MINUTES,
+        'LOGIN_FAIL_MAX' => LOGIN_FAIL_MAX,
+        'DEMO' => DEMO,
+        'LANGUAGE' => include('../helpers/get_language.php'),
+        'TIMEZONE' => TIMEZONE,
+        'SMTP' => 'php',
+        'EMAIL_ADDRESS_BCC' => '',
+        'MAIL_SENDINBLUE_API_KEY' => '',
+        'SMTP_DEBUG' => false,
+        'SMTP_HOST' => '',
+        'SMTP_USERNAME' => '',
+        'SMTP_PASSWORD' => '',
+        'SMTP_PORT' => 587,
+        'SMTP_ENCRYPTION' => 'tls'
+    ];
+
+    $write_config_file = include '../modules/write_config_file.php';
+    return $write_config_file($config);
+};
