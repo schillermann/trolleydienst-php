d065524d3ff064f07fdccc023f17171a6b9a3b6a
user-kh <anonym>
2022-07-15 22:20:54 +0200

    Reperatur Fehler bei der Teilnehmer-Suche

--- templates/pages/user.php
+++ templates/pages/user.php
@@ -15,7 +15,8 @@
 <div class="table-container">
     <table>
         <tr>
-            <th><?= __('Name') ?></th>
+            <th><?= __('First Name') ?></th>
+            <th><?= __('Last Name') ?></th>
             <th><?= __('Email') ?></th>
             <th><?= __('Active') ?></th>
             <th><?= __('Admin') ?></th>
@@ -23,14 +24,15 @@
             <th><?= __('Action') ?></th>
         </tr>
         <?php foreach ($placeholder['user_list'] as $user) : ?>
-        <tr>
-            <td><?= $user['name'];?></td>
-            <td><?= $user['email'];?></td>
-            <td><i class="fa <?= ($user['is_active']) ? 'fa-check' : 'fa-times';?>"></i></td>
-            <td><i class="fa <?= ($user['is_admin']) ? 'fa-check' : 'fa-times';?>"></i></td>
-            <td><?= $user['last_login'];?></td>
-            <td><a class="button" href="./user-edit.php?id_user=<?= $user['id_user'];?>"><i class="fa fa-pencil"></i> <?= __('Edit') ?></a></td>
-        </tr>
+            <tr>
+                <td><?= $user['first_name']; ?></td>
+                <td><?= $user['last_name']; ?></td>
+                <td><?= $user['email']; ?></td>
+                <td><i class="fa <?= ($user['active']) ? 'fa-check' : 'fa-times'; ?>"></i></td>
+                <td><i class="fa <?= ($user['administrative']) ? 'fa-check' : 'fa-times'; ?>"></i></td>
+                <td><?= $user['logged_on']; ?></td>
+                <td><a class="button" href="./user-edit.php?id_user=<?= $user['id']; ?>"><i class="fa fa-pencil"></i> <?= __('Edit') ?></a></td>
+            </tr>
         <?php endforeach ?>
     </table>
 </div>
