5952b6e60601f6abaaaa6a74bf79097416acd610
user-kh <hendrik_kiesl@hotmail.de>
2022-07-25 21:45:16 +0200

    Variable SMTP is not declared. Please check if you meant this variable.

--- modules/send_email_phpmail.php
+++ modules/send_email_phpmail.php
@@ -1,7 +1,7 @@
 <?php
 
 return function (string $to, string $subject, string $message): string {
-    if ( SMTP == "php" ) {
+    if ( EMAIL_DISPATCH == "phpmail" ) {
         $headers   = [];
         $headers[] = 'Content-type: text/plain; charset=utf-8';
         $headers[] = 'From: =?UTF-8?B?'.base64_encode(APPLICATION_NAME . ' - ' . CONGREGATION_NAME).'?=<' . EMAIL_ADDRESS_FROM . '>';
