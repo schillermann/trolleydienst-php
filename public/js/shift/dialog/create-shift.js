15ad2db75c10b8915621893851cdc5b198086c61
Mario Schillermann <mario.schillermann@gmail.com>
2023-06-03 10:35:05 +0200

    add prettier tool

--- public/js/shift/dialog/create-shift.js
+++ public/js/shift/dialog/create-shift.js
@@ -1,10 +1,10 @@
-"use strict"
+"use strict";
 
-import DialogButton from "../../dialog/button.js"
-import DialogButtonPrimary from "../../dialog/button-primary.js"
-import Dictionary from "../../dictionary.js"
+import DialogButton from "../../dialog/button.js";
+import DialogButtonPrimary from "../../dialog/button-primary.js";
+import Dictionary from "../../dictionary.js";
 
-const template = document.createElement('template');
+const template = document.createElement("template");
 template.innerHTML = `
     <style>
         input {
@@ -57,139 +57,147 @@ template.innerHTML = `
 `;
 // TODO: implement calculateShiftTimeTo function
 export default class ShiftDialogNewShift extends HTMLElement {
-  #shiftTypeId
-    constructor() {
-        super();
+  #shiftTypeId;
+  constructor() {
+    super();
 
-      this.#shiftTypeId = 0
-        this._shadowRoot = this.attachShadow({ mode: 'open' })
-        this._shadowRoot.appendChild(template.content.cloneNode(true))
+    this.#shiftTypeId = 0;
+    this._shadowRoot = this.attachShadow({ mode: "open" });
+    this._shadowRoot.appendChild(template.content.cloneNode(true));
 
-        this.dictionary = new Dictionary({
-            "Create Shift": {
-                de: "Schicht Anlegen"   
-            },
-            "Required": {
-                de: "Pflichtfeld"
-            },
-            "Route": {
-                de: "Route"
-            },
-            "Date": {
-                de: "Datum"
-            },
-            "From" : {
-                de: "Von"
-            },
-            "Shifts" : {
-                de: "Schichten"
-            },
-            "Shift Length in Hours": {
-                de: "Schichtlänge in Stunden"
-            },
-            "What is the name of this location?": {
-                de: "Wie heißt die Route?"
-            },
-            "To": {
-                de: "Bis"
-            },
-            "End Date": {
-                de: "Terminserie bis zum"
-            },
-            "Colour": {
-                de: "Farbe"
-            },
-            "Create": {
-                de: "Anlegen"   
-            },
-            "Cancel": {
-                de: "Abbrechen"   
-            }
-        })
+    this.dictionary = new Dictionary({
+      "Create Shift": {
+        de: "Schicht Anlegen",
+      },
+      Required: {
+        de: "Pflichtfeld",
+      },
+      Route: {
+        de: "Route",
+      },
+      Date: {
+        de: "Datum",
+      },
+      From: {
+        de: "Von",
+      },
+      Shifts: {
+        de: "Schichten",
+      },
+      "Shift Length in Hours": {
+        de: "Schichtlänge in Stunden",
+      },
+      "What is the name of this location?": {
+        de: "Wie heißt die Route?",
+      },
+      To: {
+        de: "Bis",
+      },
+      "End Date": {
+        de: "Terminserie bis zum",
+      },
+      Colour: {
+        de: "Farbe",
+      },
+      Create: {
+        de: "Anlegen",
+      },
+      Cancel: {
+        de: "Abbrechen",
+      },
+    });
+  }
+
+  /**
+   * @param {Event} event
+   * @returns {void}
+   */
+  onClickButtonCancel(event) {
+    this.setAttribute("open", "false");
+  }
+
+  /**
+   * @param {Event} event
+   * @returns {void}
+   */
+  async onClickButtonCreate(event) {
+    console.log(this._shadowRoot.getElementById("time_from").value);
+    const response = await fetch("/api/shift/create-shifts", {
+      method: "POST",
+      body: JSON.stringify({
+        startDate:
+          this._shadowRoot.getElementById("date_from").value +
+          " " +
+          this._shadowRoot.getElementById("time_from").value,
+        shiftTypeId: this.#shiftTypeId,
+        routeName: this._shadowRoot.getElementById("route").value,
+        numberOfShifts: Number(
+          this._shadowRoot.getElementById("number_of_shifts").value
+        ),
+        minutesPerShift:
+          this._shadowRoot.getElementById("hours_per_shift").value * 60,
+        color: this._shadowRoot.getElementById("color_hex").value,
+      }),
+    });
+
+    if (response.status === 201) {
+      this.setAttribute("open", "false");
+    }
+  }
+
+  async connectedCallback() {
+    customElements.get("dialog-button-primary") ||
+      window.customElements.define(
+        "dialog-button-primary",
+        DialogButtonPrimary
+      );
+    customElements.get("dialog-button") ||
+      window.customElements.define("dialog-button", DialogButton);
+
+    this._shadowRoot
+      .getElementById("button-cancel")
+      .addEventListener("click", this.onClickButtonCancel.bind(this));
+
+    this._shadowRoot
+      .getElementById("button-create")
+      .addEventListener("click", this.onClickButtonCreate.bind(this));
+  }
+
+  disconnectedCallback() {
+    this._shadowRoot
+      .getElementById("button-cancel")
+      .removeEventListener("click", this.onClickButtonCancel);
+
+    this._shadowRoot
+      .getElementById("button-create")
+      .removeEventListener("click", this.onClickButtonCreate);
+  }
+
+  static get observedAttributes() {
+    return ["open", "language-code", "shift-type-id"];
+  }
+
+  attributeChangedCallback(name, oldVal, newVal) {
+    if (name === "open") {
+      const dialog = this._shadowRoot.querySelector("dialog");
+      if (newVal === "true") {
+        dialog.showModal();
+        return;
+      }
+      dialog.close();
+      return;
     }
 
-    /**
-     * @param {Event} event
-     * @returns {void}
-     */
-    onClickButtonCancel(event) {
-      this.setAttribute("open", "false")
+    if (name === "language-code") {
+      this._shadowRoot.innerHTML = this.dictionary.innerHTMLEnglishTo(
+        newVal,
+        this._shadowRoot.innerHTML
+      );
+      return;
     }
 
-    /**
-     * @param {Event} event
-     * @returns {void}
-     */
-    async onClickButtonCreate(event) {
-      console.log(this._shadowRoot.getElementById("time_from").value)
-        const response = await fetch(
-          "/api/shift/create-shifts",
-          {
-            method: "POST", body: JSON.stringify({
-              "startDate": this._shadowRoot.getElementById("date_from").value + " " + this._shadowRoot.getElementById("time_from").value,
-              "shiftTypeId": this.#shiftTypeId,
-              "routeName": this._shadowRoot.getElementById("route").value,
-              "numberOfShifts": Number(this._shadowRoot.getElementById("number_of_shifts").value),
-              "minutesPerShift": this._shadowRoot.getElementById("hours_per_shift").value * 60,
-              "color": this._shadowRoot.getElementById("color_hex").value
-            })
-          }
-        )
-
-        if (response.status === 201) {
-          this.setAttribute("open", "false")
-        }
+    if (name === "shift-type-id") {
+      this.#shiftTypeId = Number(newVal);
     }
-
-    async connectedCallback() {
-      customElements.get('dialog-button-primary') || window.customElements.define('dialog-button-primary', DialogButtonPrimary)
-      customElements.get('dialog-button') || window.customElements.define('dialog-button', DialogButton)
-
-      this._shadowRoot.getElementById("button-cancel").addEventListener(
-        "click",
-        this.onClickButtonCancel.bind(this)
-      )
-
-      this._shadowRoot.getElementById("button-create").addEventListener(
-        "click",
-        this.onClickButtonCreate.bind(this)
-      )
-    }
-
-    disconnectedCallback() {
-      this._shadowRoot.getElementById("button-cancel").removeEventListener(
-        "click",
-        this.onClickButtonCancel
-      )
-
-      this._shadowRoot.getElementById("button-create").removeEventListener(
-        "click",
-        this.onClickButtonCreate
-      )
-    }
-
-    static get observedAttributes() {
-        return ["open", "language-code", "shift-type-id"];
-    }
-    
-    attributeChangedCallback(name, oldVal, newVal) {
-        if (name === "open") {
-            const dialog = this._shadowRoot.querySelector("dialog")
-            if (newVal === "true") {
-                dialog.showModal()
-                return
-            }
-            dialog.close()
-            return
-        }
-
-        if (name === "language-code") {
-            this._shadowRoot.innerHTML = this.dictionary.innerHTMLEnglishTo(newVal, this._shadowRoot.innerHTML)
-            return
-        }
-
-        if (name === "shift-type-id") {
-          this.#shiftTypeId = Number(newVal)
-        }
-    }
+  }
 }
