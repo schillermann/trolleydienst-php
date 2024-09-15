9cdf8f28d34163594d58a4116129f608839072f3
Mario Schillermann <mario.schillermann@gmail.com>
2024-09-15 20:44:57 +0200

    clean up publishers page

--- src/PublishersPage.php
+++ src/PublishersPage.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App;
 
 use PhpPages\OutputInterface;
@@ -9,15 +10,6 @@ class PublishersPage implements PageInterface
     public function viaOutput(OutputInterface $output): OutputInterface
     {
         $placeholder = require '../includes/init_page.php';
-
-        if ( isset($_POST["user_search"]) ) {
-            $user_list = Tables\Publisher::select_user_search_name($database_pdo, $_POST["user_search"]);
-        } else {
-            $user_list = Tables\Publisher::select_all($database_pdo);
-        }
-
-        $placeholder['user_list'] = $user_list;
-
         $render_page = include '../includes/render_page.php';
 
         return $output
